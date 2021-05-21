221e45d68dcd344c3e9548eb923e4ed50b881f0b
Adreena Fulcher <hurlocene@gmail.com>
2021-05-21 16:18:16 -0400

    maybe this works

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let button = document.getElementById("new-task-description")
-let task = document.getElementById("create-task-form")
-//let submit = document.getElementById("submit")
-let newTaskPro = document.getElementById("new-task-priority")
+  let task = document.getElementById("create-task-form")
+  //let submit = document.getElementById("submit")
+  let newTaskPro = document.getElementById("new-task-priority")
 
-let newTaskUl = document.getElementById("tasks")
+  let newTaskUl = document.getElementById("tasks")
 
-task.addEventListener("submit", createNewTask);
+  task.addEventListener("submit", createNewTask);
 });
 
 let createNewTask = event => {
-event.preventDefault();
+  event.preventDefault();
 
-let button = document.getElementById("new-task-description");
-let newTask = document.createElement("li");
+  let button = document.getElementById("new-task-description");
+  let newTask = document.createElement("li");
 
-newTask.innerText = button.value
-}
+  newTask.innerText = button.value
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+let appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task)
+};
